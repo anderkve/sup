76b5e795393e0cc2262759911b91ba75b6342f77
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2020-12-27 20:26:58 +0100

    Added hist1d mode. Small fix to hist2d mode.
    	modified:   sup.py
    	new file:   sup/hist1dmode.py
    	modified:   sup/hist2dmode.py

--- sup.py
+++ sup.py
@@ -26,7 +26,7 @@ examples:
 import sys
 import os
 import argparse
-from sup import listmode, colorsmode, plr2dmode, plr1dmode, maxmin2dmode, maxmin1dmode, avg1dmode, avg2dmode, hist2dmode, post2dmode, graph1dmode, graph2dmode
+from sup import listmode, colorsmode, plr2dmode, plr1dmode, maxmin2dmode, maxmin1dmode, avg1dmode, avg2dmode, hist1dmode, hist2dmode, post2dmode, graph1dmode, graph2dmode
 
 
 
@@ -47,6 +47,7 @@ def main():
         epilog="""
 modes:
   sup list      list dataset names and indices
+  sup hist1d    plot the x histogram
   sup hist2d    plot the (x,y) histogram
   sup post2d    plot the (x,y) posterior probability distribution
   sup max1d     plot the maximum y value across the x axis
@@ -83,6 +84,28 @@ examples:
     parser_listmode = subparsers.add_parser("list")
     parser_listmode.set_defaults(func=listmode.run)
     parser_listmode.add_argument("input_file", type=str, action="store", help="path to the input data file")
+
+    # Parser for "hist1d" mode
+    parser_hist1dmode = subparsers.add_parser("hist1d")
+    parser_hist1dmode.set_defaults(func=hist1dmode.run)
+    parser_hist1dmode.add_argument("input_file", type=str, action="store", help="path to the input data file")
+    parser_hist1dmode.add_argument("x_index", type=int, action="store", help="index of the x-axis dataset")
+    parser_hist1dmode.add_argument("-f", "--filter", nargs="+", type=int, action="store", dest="filter_indices", default=None, help="indices of boolean datasets used for filtering", metavar="F_INDEX")
+    parser_hist1dmode.add_argument("-w", "--weights", type=int, action="store", dest="w_index", default=None, help="index of the weights dataset", metavar="W_INDEX")
+    parser_hist1dmode.add_argument("-n", "--normalise", action="store_true", dest="normalise_histogram", default=False, help="normalise histogram to integrate to unity")
+    parser_hist1dmode.add_argument("-xr", "--x-range", nargs=2, type=float, action="store", dest="x_range", default=None, help="x-axis range", metavar=("X_MIN", "X_MAX"))
+    parser_hist1dmode.add_argument("-yr", "--y-range", nargs=2, type=float, action="store", dest="y_range", default=None, help="y-axis range", metavar=("Y_MIN", "Y_MAX"))
+    parser_hist1dmode.add_argument("-b", "--bins", nargs=2, type=int, action="store", dest="xy_bins", default=None, help="number of bins for each axis", metavar=("X_BINS", "Y_BINS"))
+    parser_hist1dmode.add_argument("-g", "--gray", action="store_true", dest="use_grayscale", default=False, help="grayscale plot")
+    parser_hist1dmode.add_argument("-wb", "--white-bg", action="store_true", dest="use_white_bg", default=False, help="white background")
+    # parser_hist1dmode.add_argument("-nc", "--num-colors", type=int, action="store", dest="n_colors", default=10, help="number of colors in colorbar (max 10)", metavar="N_COLORS")
+    # parser_hist1dmode.add_argument("-cm", "--colormap", type=int, action="store", dest="cmap_index", default=0, help="select colormap: 0 = viridis-ish, 1 = jet-ish", metavar="CM")
+    # parser_hist1dmode.add_argument("-rc", "--reverse-colormap", action="store_true", dest="reverse_colormap", default=False, help="reverse colormap")
+    parser_hist1dmode.add_argument("-xt", "--x-transf", type=str, action="store", dest="x_transf_expr", default="", help="tranformation for the x-axis dataset, using numpy as 'np' (e.g. -xt \"np.log10(x)\")", metavar="EXPR")
+    parser_hist1dmode.add_argument("-yt", "--y-transf", type=str, action="store", dest="y_transf_expr", default="", help="tranformation for the y-axis dataset, using numpy as 'np' (e.g. -yt \"np.log10(y)\")", metavar="EXPR")
+    parser_hist1dmode.add_argument("-wt", "--w-transf", type=str, action="store", dest="w_transf_expr", default="", help="tranformation for the weights dataset, using numpy as 'np' (e.g. -zt \"np.ones(w.shape\")", metavar="EXPR")
+    parser_hist1dmode.add_argument("-rs", "--read-slice", nargs=3, type=int, action="store", dest="read_slice", default=[0,-1,1], help="read only the given slice of each dataset", metavar=("START", "END", "STEP"))
+    parser_hist1dmode.add_argument("-d", "--decimals", type=int, action="store", dest="n_decimals", default=2, help="set the number of decimals for axis and colorbar tick labels", metavar="N_DECIMALS")
 
     # Parser for "hist2d" mode
     parser_hist2dmode = subparsers.add_parser("hist2d")
--- sup/hist1dmode.py
+++ sup/hist1dmode.py
@@ -3,7 +3,6 @@ import numpy as np
 import h5py
 import sup.defaults as defaults
 import sup.utils as utils
-from collections import OrderedDict
 
 
 #
@@ -19,55 +18,86 @@ bg_ccode_wb, fg_ccode_wb = 231, 16  # 231, 232
 bg_ccode = bg_ccode_bb
 fg_ccode = fg_ccode_bb
 
-regular_marker = " ■"
+# regular_marker_up = " ▀"
+regular_marker_up = " █"
+regular_marker_down = " ▄"
+
+# regular_marker = " ●"
+# regular_marker = " ▁"
+# regular_marker = " ▔"
+
 special_marker = " 🟊"  # " ★" " 🟊" " ✱"
 
-empty_bin_marker_grayscale = " □"
-empty_bin_marker_color = " □"
+# fill_marker = "  "
+# fill_marker = " ■"
+fill_marker = " █"
+
+empty_bin_marker_grayscale = "  "
+# empty_bin_marker_grayscale = " ▁"
+# empty_bin_marker_grayscale = " □"
+# empty_bin_marker_grayscale = " ■"
+empty_bin_marker_color = "  "
+# empty_bin_marker_color = " ▁"
+# empty_bin_marker_color = " □"
+# empty_bin_marker_color = " ■"
 empty_bin_marker = empty_bin_marker_color
 
-empty_bin_ccode_grayscale_bb = 237
-empty_bin_ccode_grayscale_wb = 252
+empty_bin_ccode_grayscale_bb = 235
+empty_bin_ccode_grayscale_wb = 253
 empty_bin_ccode_grayscale = empty_bin_ccode_grayscale_bb
 
-empty_bin_ccode_color_bb = 237
-empty_bin_ccode_color_wb = 252
+empty_bin_ccode_color_bb = 235
+empty_bin_ccode_color_wb = 253
 empty_bin_ccode = empty_bin_ccode_color_bb
 
-cmaps_grayscale = [
-    [233, 236, 239, 242, 244, 247, 250, 253, 255, 231],  # for black background
-    [232, 235, 238, 240, 243, 246, 248, 251, 253, 255][::-1],  # for white background
-]
-ccodes_grayscale = cmaps_grayscale[0]
+max_bin_ccode_grayscale_bb = 231
+max_bin_ccode_grayscale_wb = 232
+max_bin_ccode_grayscale = max_bin_ccode_grayscale_bb
 
-cmaps = [
-    [53,56,62,26,31,36,42,47,154,226],      # viridis-ish
-    [18,20,27,45,122,155,226,214,202,196],  # jet-ish
-]
-ccodes = cmaps[0]
+max_bin_ccode_color_bb = 231
+max_bin_ccode_color_wb = 232
+max_bin_ccode = max_bin_ccode_color_bb
+
+fill_bin_ccode_grayscale_bb = empty_bin_ccode_grayscale_bb
+fill_bin_ccode_grayscale_wb = empty_bin_ccode_grayscale_wb
+fill_bin_ccode_grayscale = empty_bin_ccode_grayscale_bb
+
+fill_bin_ccode_color_bb = empty_bin_ccode_color_bb
+fill_bin_ccode_color_wb = empty_bin_ccode_color_wb
+fill_bin_ccode = fill_bin_ccode_color_bb
+
+ccode_grayscale_bb = 231
+ccode_grayscale_wb = 232
+ccode_grayscale = ccode_grayscale_bb
+
+ccode_color_bb = 4 # 2  # 231
+ccode_color_wb = 12 # 10 # 232
+ccode = ccode_color_bb
+
+def get_color_code(z_val):
+
+    if z_val in [1,2]:
+        return ccode
+    elif z_val == -1:
+        return ccode
+    elif z_val == 0:
+        return empty_bin_ccode
+    else:
+        raise Exception("Unexpected z_val. This shouldn't happen...")
 
 
+def get_marker(z_val):
 
-def get_color_code(z_val, z_norm, color_z_lims):
-
-    # print("DEBUG: z_val:", z_val, "   z_norm:", z_norm)
-    if z_norm == 1.0:
-        return ccodes[-1]
-    elif z_norm == 0.0:
-        return ccodes[0]
-
-    i = 0
-    for j, lim in enumerate(color_z_lims):
-        if z_val >= lim:
-            i = j
-        else:
-            break
-    return ccodes[i]
-
-
-def get_marker(z_norm):
-
-    return regular_marker
+    if z_val == 2:
+        return regular_marker_up
+    elif z_val == 1:
+        return regular_marker_down
+    elif z_val == -1:
+        return fill_marker
+    elif z_val == 0:
+        return empty_bin_marker
+    else:
+        raise Exception("Unexpected z_val. This shouldn't happen...")
 
 
 #
@@ -76,24 +106,24 @@ def get_marker(z_norm):
 
 def run(args):
 
-    assert args.cmap_index in range(len(cmaps))
-
-    global ccodes 
-    global ccodes_grayscale 
+    global ccode
+    global ccode_grayscale 
     global bg_ccode
     global fg_ccode
+    global max_bin_ccode
+    global max_bin_ccode_grayscale
     global empty_bin_ccode
     global empty_bin_ccode_grayscale
+    global fill_bin_ccode
+    global fill_bin_ccode_grayscale
     global empty_bin_marker
     global special_marker
-    global color_z_lims
     global ff
     global ff2
 
     input_file = args.input_file
 
     x_index = args.x_index
-    y_index = args.y_index
 
     w_index = args.w_index
     use_weights = bool(w_index is not None)
@@ -106,44 +136,33 @@ def run(args):
     x_range = args.x_range
     y_range = args.y_range
 
-    z_range_user = args.z_range
-    user_defined_z_range = bool(z_range_user is not None)
-
     read_slice = slice(*args.read_slice)
 
     xy_bins = args.xy_bins
     if not xy_bins:
         xy_bins = defaults.xy_bins
     
-    cmap_index = args.cmap_index
-    ccodes = cmaps[cmap_index]
     use_white_bg = args.use_white_bg
     if use_white_bg:
         bg_ccode = bg_ccode_wb
         fg_ccode = fg_ccode_wb
         empty_bin_ccode = empty_bin_ccode_color_wb
-        # ccodes = ccodes_color_wb
+        max_bin_ccode = max_bin_ccode_color_wb
+        fill_bin_ccode = fill_bin_ccode_color_wb
+        ccode = ccode_color_wb
 
     if args.use_grayscale:
         if use_white_bg:
-            ccodes = cmaps_grayscale[1]
-            # ccodes = ccodes_grayscale_wb
+            ccode = ccode_grayscale_wb
+            max_bin_ccode = max_bin_ccode_grayscale_wb
             empty_bin_ccode = empty_bin_ccode_grayscale_wb
+            fill_bin_ccode = fill_bin_ccode_grayscale_wb
         else:
-            ccodes = cmaps_grayscale[0]
-            # ccodes = ccodes_grayscale_bb
+            ccode = ccode_grayscale_bb
+            max_bin_ccode = max_bin_ccode_grayscale_bb
             empty_bin_ccode = empty_bin_ccode_grayscale_bb
+            fill_bin_ccode = fill_bin_ccode_grayscale_bb
         empty_bin_marker = empty_bin_marker_grayscale
-
-    n_colors = args.n_colors
-    if n_colors < 1:
-        n_colors = 1
-    elif n_colors > 10:
-        n_colors = 10
-    ccodes = [ ccodes[int(i)] for i in np.round( np.linspace(0, len(ccodes)-1, n_colors) ) ]
-
-    if args.reverse_colormap:
-        ccodes = ccodes[::-1]
 
     n_decimals = args.n_decimals
     ff = "{: ." + str(n_decimals) + "e}"
@@ -158,10 +177,8 @@ def run(args):
 
     dset_names = utils.get_dataset_names(f)
     x_name = dset_names[x_index]
-    y_name = dset_names[y_index]
 
     x_data = np.array(f[x_name])[read_slice]
-    y_data = np.array(f[y_name])[read_slice]
 
     w_name = None
     w_data = np.ones(len(x_data))
@@ -173,74 +190,48 @@ def run(args):
 
     f.close()
 
-    assert len(x_data) == len(y_data)
     assert len(x_data) == len(w_data)
     # data_length = len(x_data)
 
     if use_filters:
-        x_data, y_data, w_data = utils.apply_filters([x_data, y_data, w_data], filter_datasets)
+        x_data, w_data = utils.apply_filters([x_data, w_data], filter_datasets)
 
     x_transf_expr = args.x_transf_expr
     y_transf_expr = args.y_transf_expr
     w_transf_expr = args.w_transf_expr
-    z_transf_expr = args.z_transf_expr
+
     # @todo: add the x,y,w variables to a dictionary of allowed arguments to eval()
     x = x_data
-    y = y_data
     w = w_data
     if x_transf_expr != "":
         x_data = eval(x_transf_expr)
-    if y_transf_expr != "":
-        y_data = eval(y_transf_expr)
     if w_transf_expr != "":
         w_data = eval(w_transf_expr)
 
     if not x_range:
         x_range = [np.min(x_data), np.max(x_data)]
-    if not y_range:
-        y_range = [np.min(y_data), np.max(y_data)]
-
 
 
     #
     # Get a dict with info per bin
     #
 
-    bins_content_unweighted,_ ,_  = np.histogram2d(x_data, y_data, bins=xy_bins, range=[x_range, y_range], density=normalise_histogram) 
-    bins_content, x_bin_limits, y_bin_limits = np.histogram2d(x_data, y_data, bins=xy_bins, range=[x_range, y_range], weights=w_data, density=normalise_histogram) 
+    bins_content_unweighted,_  = np.histogram(x_data, bins=xy_bins[0], range=x_range, density=normalise_histogram) 
+    bins_content, x_bin_limits = np.histogram(x_data, bins=xy_bins[0], range=x_range, weights=w_data, density=normalise_histogram) 
 
-    # Apply z-axis transformation
-    z = bins_content
-    if z_transf_expr != "":
-        bins_content = eval(z_transf_expr)
+    # Apply y-axis transformation
+    y = bins_content
+    if y_transf_expr != "":
+        bins_content = eval(y_transf_expr)
 
     dx = x_bin_limits[1] - x_bin_limits[0]
-    dy = y_bin_limits[1] - y_bin_limits[0]
-
     x_bin_centres = x_bin_limits[:-1] + 0.5 * dx
-    y_bin_centres = y_bin_limits[:-1] + 0.5 * dy
 
-    bins_info = OrderedDict()
-    for i,xi in enumerate(x_bin_centres):
-        for j,yj in enumerate(y_bin_centres):
+    if not y_range:
+        y_range = [np.min(bins_content[bins_content > -np.inf]), np.max(bins_content)]
+    y_min, y_max = y_range
 
-            bin_key = (i,j)
-            z_val = bins_content[bin_key]      
-            bin_count = bins_content_unweighted[bin_key]
-
-            if bin_count > 0:
-                bins_info[bin_key] = (xi, yj, z_val)
-
-    # Get z max and minimum from data if not set by the user
-    z_range = None
-    if user_defined_z_range:
-        z_range = z_range_user
-    else:
-        z_range = [np.min(bins_content[bins_content > -np.inf]), np.max(bins_content)]
-    z_min, z_max = z_range 
-
-    # Set color limits
-    color_z_lims = list( np.linspace(z_min, z_max, len(ccodes)+1) )
+    bins_info, x_bin_limits, y_bin_limits = utils.get_bin_tuples_avg_1d(x_bin_centres, np.minimum(bins_content, y_max), xy_bins, x_range, y_range, fill_below=True, split_marker=True)
 
 
     #
@@ -257,24 +248,17 @@ def run(args):
 
             xiyi = (xi,yi)
 
-            ccode = empty_bin_ccode
+            cc = empty_bin_ccode
             marker = empty_bin_marker
 
             if xiyi in bins_info.keys():
                 z_val = bins_info[xiyi][2]
-                z_norm = 0.0
-                if (z_max != z_min):
-                    z_norm = (z_val - z_min) / (z_max - z_min)
-                if z_norm > 1.0:
-                    z_norm = 1.0
-                elif z_norm < 0.0:
-                    z_norm = 0.0
 
-                ccode = get_color_code(z_val, z_norm, color_z_lims)
-                marker = get_marker(z_norm)
+                cc = get_color_code(z_val)
+                marker = get_marker(z_val)
 
             # Add point to line
-            yi_line += utils.prettify(marker, ccode, bg_ccode)
+            yi_line += utils.prettify(marker, cc, bg_ccode)
 
         plot_lines.append(yi_line)
 
@@ -286,20 +270,42 @@ def run(args):
 
     # Add axes
     axes_mod_func = lambda input_str : utils.prettify(input_str, fg_ccode, bg_ccode, bold=True)
-    plot_lines = utils.add_axes(plot_lines, xy_bins, x_bin_limits, y_bin_limits, mod_func=axes_mod_func, floatf=ff)
+    fill_mod_func = lambda input_str : utils.prettify(input_str, empty_bin_ccode, bg_ccode, bold=True)
+    plot_lines = utils.add_axes(plot_lines, xy_bins, x_bin_limits, y_bin_limits, mod_func=axes_mod_func, mod_func_2=fill_mod_func, floatf=ff, add_y_grid_lines=True)
 
     # Add blank top line
     plot_lines, fig_width = utils.insert_line("", 0, plot_lines, fig_width, fg_ccode, bg_ccode, insert_pos=0)
 
 
-    #
-    # Add colorbar, legend, etc
-    #
+    # #
+    # # Add legend
+    # #
 
-    plot_lines, fig_width = utils.generate_colorbar(plot_lines, fig_width, ff,
-                                                    ccodes, color_z_lims, 
-                                                    fg_ccode, bg_ccode, empty_bin_ccode)
-        
+    # # max/min legend
+    # legend_mod_func = lambda input_str, input_fg_ccode : utils.prettify(input_str, input_fg_ccode, bg_ccode, bold=True)
+
+    # point_str = ""
+    # if s_index != y_index:
+    #     point_str = "sort_" + s_type + " point: (x, y, sort) = "
+    # else:
+    #     point_str = "y_" + s_type + " point: (x, y) = "
+
+    # legend_maxmin_entries = []
+
+    # marker_str = ""
+
+    # if s_index != y_index:
+    #     point = ("(" + ff2 + ", " + ff2 + ", " + ff2 + ")").format(xys_maxmin[0], xys_maxmin[1], xys_maxmin[2])
+    # else:
+    #     point = ("(" + ff2 + ", " + ff2 + ")").format(xys_maxmin[0], xys_maxmin[1])
+
+    # point_str += point
+    # legend_maxmin_entries.append( (marker_str, fg_ccode, point_str, fg_ccode) )
+    # legend_maxmin, legend_maxmin_width = utils.generate_legend(legend_maxmin_entries, legend_mod_func, sep="  ", internal_sep=" ")
+
+    # plot_lines, fig_width = utils.insert_line("", 0, plot_lines, fig_width, fg_ccode, bg_ccode)
+    # plot_lines, fig_width = utils.insert_line(legend_maxmin, legend_maxmin_width, plot_lines, fig_width, fg_ccode, bg_ccode)
+
 
     #
     # Add left padding
@@ -314,9 +320,9 @@ def run(args):
     #
 
     x_label = x_name
-    y_label = y_name
-    z_label = "bin height"
+    y_label = "bin height"
     w_label = w_name
+
 
     #
     # Add info text
@@ -325,10 +331,8 @@ def run(args):
     info_lines = utils.generate_info_text(ff2,
                                           x_label, x_range, 
                                           y_label=y_label, y_range=y_range, 
-                                          z_label=z_label, z_range=z_range, 
                                           x_transf_expr=x_transf_expr, 
                                           y_transf_expr=y_transf_expr,
-                                          z_transf_expr=z_transf_expr,
                                           w_label=w_label,
                                           w_transf_expr=w_transf_expr,
                                           filter_names=filter_names,
