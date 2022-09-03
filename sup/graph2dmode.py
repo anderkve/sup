57000206f509493b03cf7589d22b99e11d2eb83d
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2022-09-03 15:25:57 +0200

    Now using CColorSettings class in graph2dmode.
    	modified:   sup/graph2dmode.py

--- sup/graph2dmode.py
+++ sup/graph2dmode.py
@@ -3,6 +3,7 @@ import numpy as np
 import sup.defaults as defaults
 import sup.utils as utils
 from sup.colors import cmaps, cmaps_grayscale
+from sup.ccodesettings import CCodeSettings
 
 
 #
@@ -15,19 +16,12 @@ special_marker = defaults.special_marker
 empty_bin_marker = defaults.empty_bin_marker_2d
 
 
-max_bin_ccode_grayscale_bb = 231
-max_bin_ccode_grayscale_wb = 232
-
-max_bin_ccode_color_bb = 231
-max_bin_ccode_color_wb = 232
-
-
-def get_color_code(ccodes, z_val, color_z_lims):
+def get_color_code(ccs, z_val, color_z_lims):
 
     if z_val >= color_z_lims[-1]:
-        return ccodes[-1]
+        return ccs.ccodes[-1]
     elif z_val <= color_z_lims[0]:
-        return ccodes[0]        
+        return ccs.ccodes[0]        
     else:
         i = 0
         for j, lim in enumerate(color_z_lims):
@@ -35,7 +29,7 @@ def get_color_code(ccodes, z_val, color_z_lims):
                 i = j
             else:
                 break
-        return ccodes[i]
+        return ccs.ccodes[i]
     raise Exception("Unexpected z_val. This shouldn't happen...")
 
 
@@ -64,36 +58,14 @@ def run(args):
     if not xy_bins:
         xy_bins = defaults.xy_bins
     
-    bg_ccode = defaults.bg_ccode_bb
-    fg_ccode = defaults.fg_ccode_bb
-
-    cmap_index = args.cmap_index
-    empty_bin_ccode = defaults.empty_bin_ccode_color_bb
-    max_bin_ccode = max_bin_ccode_color_bb
-    ccodes = cmaps[cmap_index]
-    use_white_bg = args.use_white_bg
-    if use_white_bg:
-        bg_ccode = defaults.bg_ccode_wb
-        fg_ccode = defaults.fg_ccode_wb
-        empty_bin_ccode = defaults.empty_bin_ccode_color_wb
-        max_bin_ccode = max_bin_ccode_color_wb
-
-    if args.use_grayscale:
-        if use_white_bg:
-            ccodes = cmaps_grayscale[1]
-            max_bin_ccode = max_bin_ccode_grayscale_wb
-            empty_bin_ccode = defaults.empty_bin_ccode_grayscale_wb
-        else:
-            ccodes = cmaps_grayscale[0]
-            max_bin_ccode = max_bin_ccode_grayscale_bb
-            empty_bin_ccode = defaults.empty_bin_ccode_grayscale_bb
-
-    ccodes = [
-        ccodes[int(i)] for i in np.round(np.linspace(0,len(ccodes)-1,args.n_colors))
-    ]
+    ccs = CCodeSettings()
+    ccs.ccodes = ccs.cmaps[args.cmap_index]
+    ccs.switch_settings(use_white_bg=args.use_white_bg, 
+                        use_grayscale=args.use_grayscale)
+    ccs.set_n_colors(args.n_colors)
 
     if args.reverse_colormap:
-        ccodes = ccodes[::-1]
+        ccs.ccodes = ccs.ccodes[::-1]
 
     n_decimals = args.n_decimals
     ff = "{: ." + str(n_decimals) + "e}"
@@ -149,7 +121,7 @@ def run(args):
     z_min, z_max = z_range
 
     # Set color limits
-    color_z_lims = list(np.linspace(z_min, z_max, len(ccodes)+1))
+    color_z_lims = list(np.linspace(z_min, z_max, len(ccs.ccodes)+1))
 
     #
     # Get a dict with info per bin
@@ -167,26 +139,23 @@ def run(args):
     fig_width = 0
     for yi in range(xy_bins[1]):
 
-        yi_line = utils.prettify(" ", fg_ccode, bg_ccode)
+        yi_line = utils.prettify(" ", ccs.fg_ccode, ccs.bg_ccode)
 
         for xi in range(xy_bins[0]):
 
             xiyi = (xi,yi)
 
-            ccode = empty_bin_ccode
+            ccode = ccs.empty_bin_ccode
             marker = empty_bin_marker
 
             if xiyi in bins_info.keys():
                 z_val = bins_info[xiyi][2]
-                # z_norm = 0.0
-                # if (z_max != z_min):
-                #     z_norm = (z_val - z_min) / (z_max - z_min)
 
-                ccode = get_color_code(ccodes, z_val, color_z_lims)
+                ccode = get_color_code(ccs, z_val, color_z_lims)
                 marker = get_marker()
 
             # Add point to line
-            yi_line += utils.prettify(marker, ccode, bg_ccode)
+            yi_line += utils.prettify(marker, ccode, ccs.bg_ccode)
 
         plot_lines.append(yi_line)
 
@@ -197,14 +166,15 @@ def run(args):
     fig_width = plot_width
 
     # Add axes
-    axes_mod_func = lambda input_str : utils.prettify(input_str, fg_ccode, 
-                                                      bg_ccode, bold=True)
+    axes_mod_func = lambda input_str : utils.prettify(input_str, ccs.fg_ccode, 
+                                                      ccs.bg_ccode, bold=True)
     plot_lines = utils.add_axes(plot_lines, xy_bins, x_bin_limits, y_bin_limits, 
                                 mod_func=axes_mod_func, floatf=ff)
 
     # Add blank top line
     plot_lines, fig_width = utils.insert_line("", 0, plot_lines, fig_width,
-                                              fg_ccode, bg_ccode, insert_pos=0)
+                                              ccs.fg_ccode, ccs.bg_ccode,
+                                              insert_pos=0)
 
 
     #
@@ -212,16 +182,16 @@ def run(args):
     #
 
     plot_lines, fig_width = utils.generate_colorbar(plot_lines, fig_width, ff,
-                                                    ccodes, color_z_lims, 
-                                                    fg_ccode, bg_ccode, 
-                                                    empty_bin_ccode)
+                                                    ccs.ccodes, color_z_lims, 
+                                                    ccs.fg_ccode, ccs.bg_ccode, 
+                                                    ccs.empty_bin_ccode)
 
         
     #
     # Add left padding
     #
 
-    plot_lines = utils.add_left_padding(plot_lines, fg_ccode, bg_ccode)
+    plot_lines = utils.add_left_padding(plot_lines, ccs.fg_ccode, ccs.bg_ccode)
 
 
     #
@@ -238,7 +208,7 @@ def run(args):
     #
 
     plot_lines, fig_width = utils.add_info_text(
-        plot_lines, fig_width, fg_ccode, bg_ccode, ff2, x_label, x_range,
+        plot_lines, fig_width, ccs.fg_ccode, ccs.bg_ccode, ff2, x_label, x_range,
         y_label=y_label, y_range=y_range, z_label=z_label, z_range=z_range, 
         mode_name="graph")
 
